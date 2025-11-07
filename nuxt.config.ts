2fc4d229adafebc70ae6fa48297eef835ed21c20
Trevor McKinney <trevmickey@gmail.com>
2025-11-06 18:16:45 -0700

    styles set up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['@/assets/styles/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "~/assets/styles/variables" as *;
+            @use "~/assets/styles/mixins" as *;
+          `,
+        },
+      },
+    },
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
